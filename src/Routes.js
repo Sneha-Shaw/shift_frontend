4b232b83176496ff90cec592eb204f639c0920d1
Sneha Shaw <snehashaw122@gmail.com>
2022-12-04 18:50:02 +0530

    `feat`:`home`

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,34 @@
 import React from "react";
-import { BrowserRouter, Routes, Route ,Redirect,Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/Login/Login'
 import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme'
-import { NavRoute } from './Layout/NavRoute'
+import Home from "./pages/Home/Home";
+import Navbar from './components/Navbar/Navbar'
 
 
 const Router = () => {
+    // doctor info from redux
+    const doctorInfo = null
     return (
         <ThemeProvider theme={theme}>
             <BrowserRouter>
                 <Routes>
-                    <Route exact path="" element={<Navigate to="/login"/>} />
                     <Route exact path="/login" element={<Login />} />
-                    
+                    <Route
+                        path="/"
+                        element={
+                            doctorInfo ? (
+                                    <>
+                                        <Navbar />
+                                        <Home />
+                                    </>                                 
+                            ) : (
+                                <Navigate to="/login" />
+                            )
+                        }
+                    />
+
                 </Routes>
             </BrowserRouter>
         </ThemeProvider>
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import useStyles from './styles'
+
+const Home = () => {
+    const classes = useStyles()
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
