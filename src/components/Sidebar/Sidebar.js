3362c70f1b1d5417c58150a83cc67b6aa71ec3fd
Sneha Shaw <snehashaw122@gmail.com>
2022-12-05 11:31:32 +0530

    `feat`:`sidebar`

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import useStyles from './styles'
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import { NavLink, Link } from 'react-router-dom';
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+    const classes = useStyles()
+    return (
+        <div className={classes.root}>
+            {/* home and self seervice icons */}
+
+            <div className={classes.icons}>
+                <NavLink to='/'
+                    style={({ isActive }) => ({
+                        background: isActive ? '#256D85' : 'none',
+                    })} className={classes.navLink}>
+                    <HomeIcon className={classes.icon} fontSize='large' />
+                    <h3>
+                        Home
+                    </h3>
+                </NavLink>
+                <NavLink to='/self-service'
+                    style={({ isActive }) => ({
+                        background: isActive ? '#256D85' : 'none',
+                    })} className={classes.navLink}>
+                    <PersonIcon fontSize="large" />
+                    <h3>
+                        Self Service
+                    </h3>
+                </NavLink>
+            </div>
+        </div>
+    )
 }
 
 export default Sidebar
---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,60 @@
+
+import { makeStyles } from "@mui/styles";
+export default makeStyles((theme) => ({
+    root: {
+        width: "5%",
+        height: "100vh",
+        backgroundColor: "#06283D",
+        position: "fixed",
+        // top: 0,
+        left: 0,
+        zIndex: 100,
+        [theme.breakpoints.down("sm")]: {
+            width: "100%",
+            height: "5%",
+        },
+    },
+    icons:{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        // justifyContent: "center",
+        height: "100%",
+        width: "100%",
+        marginTop: "1rem",
+        [theme.breakpoints.down("sm")]: {
+            flexDirection: "row",
+        },
+    },
+
+    navLink: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        // height: "100%",
+        width: "100%",
+        textDecoration: "none",
+        color: "white",
+        marginTop: "1rem",
+        padding: "1rem 0",
+        "&:hover": {
+            background: "#256D85",
+        },
+        '& svg':{
+            marginBottom: "0.5rem",
+        },
+        [theme.breakpoints.down("sm")]: {
+            flexDirection: "row",
+        },
+    },
+    icon: {
+        color: "white",
+        [theme.breakpoints.down("sm")]: {
+            display: "none",
+        },
+    },
+
+
+    
+}))
