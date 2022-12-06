51e0b2ac7bae4fd71c1eb9843aa089879f619069
Sneha Shaw <snehashaw122@gmail.com>
2022-12-06 20:41:42 +0530

    `feat`:`leave list`

--- src/pages/Leave/Leave.js
+++ src/pages/Leave/Leave.js
@@ -2,15 +2,90 @@ import React from 'react'
 import useStyles from './styles'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import SubSidebar from '../../components/SubSidebar/SubSidebar'
+import AddIcon from '@mui/icons-material/Add';
+import { Button } from '@mui/material';
 
 const Leave = () => {
     const classes = useStyles()
-  return (
-    <div className={classes.root}>
-      <Sidebar />
-        <SubSidebar />
-    </div>
-  )
+    // array of leave duration start date end date type of leave and leave declined or approved
+    const leave = [
+        {
+            startDate: "Mon, 5th Dec",
+            endDate: "Mon, 5th Dec",
+            duration: "1 Day",
+            type: "Casual",
+            status: "awaiting"
+        },
+        {
+            startDate: "Sun, 4th Dec",
+            endDate: "Sun, 4th Dec",
+            duration: "1 Day",
+            type: "Sick",
+            status: "approved"
+        },
+        {
+            startDate: "Sat, 3rd Dec",
+            endDate: "Sat, 3rd Dec",
+            duration: "1 Day",
+            type: "Casual",
+            status: "declined"
+        }
+    ]
+    return (
+        <div className={classes.root}>
+            <Sidebar />
+            <SubSidebar />
+            <div className={classes.main}>
+                <div className={classes.main__header}>
+                    <h1>Leave</h1>
+                    <div className={classes.main__header__buttons}>
+                        <Button
+                            variant="contained"
+                            startIcon={<AddIcon />}
+                            className={classes.main__header__buttons__add}
+                        >
+                            Add Leave
+                        </Button>
+                    </div>
+                </div>
+                <div className={classes.main__content}>
+                    {
+                        leave.map((item, index) => (
+                            <div className={classes.main__content__item} key={index}>
+                                <div className={classes.main__content__item__left}>
+                                    <div>
+                                    <h2>{item.duration} Application</h2>
+                                    <p
+                                    style={{
+                                        color: item.type === "Casual" ? "#F0AB00" : "#4BACE8",
+                                        border: item.type=== "Casual"? "1px solid #F0AB00": "1px solid #4BACE8",
+                                        padding: "0.3rem",
+                                        borderRadius: ".5rem",
+                                    }}
+                                    >{item.type}</p>
+                                    </div>
+                                    <p>{item.startDate} - {item.endDate}</p>
+                                </div>
+                                <div className={classes.main__content__item__right}>
+                                    <Button variant="contained"
+                                    sx={{
+                                        background: item.status === "awaiting" ? "#FCEFCF" : item.status === "approved" ? "#D4FFE7" : "#FEEFED",
+                                        color: item.status === "awaiting" ? "#F0AB00" : item.status === "approved" ? "#00B74A" : "#FF0000",
+                                        '&:hover':{
+                                            background: item.status === "awaiting" ? "#FCEFCF" : item.status === "approved" ? "#D4FFE7" : "#FEEFED",
+                                            color: item.status === "awaiting" ? "#F0AB00" : item.status === "approved" ? "#00B74A" : "#FF0000",
+                                        }
+                                    }}
+                                    >{item.status}</Button>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Leave
--- src/pages/Leave/styles.js
+++ src/pages/Leave/styles.js
@@ -1,10 +1,110 @@
 
 import { makeStyles } from "@mui/styles";
 export default makeStyles((theme) => ({
-   root:{
+    root: {
         width: "100%",
-        height: "100vh",    
+        height: "100vh",
         display: "flex",
-            
-   }
+
+    },
+    main: {
+        width: "55%",
+        height: "60%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        // justifyContent: "center",
+        margin: "3rem auto",
+        // background: "#f7f7f7",
+        borderRadius: "1rem",
+        padding: "2rem",
+        '&>h1': {
+            fontSize: "3rem",
+            fontWeight: "bold",
+            marginBottom: "2rem",
+
+        },
+        '&>p': {
+            fontSize: "1.5rem",
+            fontWeight: "bold",
+            marginBottom: "2rem",
+        },
+    },
+
+    main__header: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        '&>h1': {
+            fontSize: "2rem",
+            fontWeight: "bold",
+        },
+        '& button': {
+            background: "#f7f7f7",
+            color: "#000",
+            '&:hover': {
+                background: "#f7f7f7",
+                color: "#000",
+            }
+        }
+    },
+    main__content: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "2rem",
+        '&>div': {
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "1rem",
+            background: "#f7f7f7",
+            borderRadius: "1rem",
+            marginBottom: "1rem",
+            border: "1px solid",
+            '&>div': {
+                display: "flex",
+                flexDirection: "column",
+                '&>h2': {
+                    fontSize: "1.5rem",
+                    fontWeight: "bold",
+                },
+                '&>p': {
+                    fontSize: "2rem",
+                    fontWeight: "bold",
+                    color: "#000",
+                },
+            },
+            '& button': {
+                boxShadow: "none",
+                '&:hover': {
+                    boxShadow: "none",
+                    cursor: "text"
+                }
+            }
+
+        }
+    },
+    main__content__item__left: {
+        display: "flex",
+        flexDirection: "column",
+        '&>div': {
+            display: "flex",
+            alignItems: "center",
+            marginBottom: "1rem",
+            width: "80%",
+            justifyContent: "space-between",
+            '&>p': {
+                fontSize: "1rem",
+                fontWeight: "bold",
+                color: "#000",
+                marginLeft: "1rem",
+            }
+        }
+    }
+
 }))
