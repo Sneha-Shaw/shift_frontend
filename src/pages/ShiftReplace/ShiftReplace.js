bfdd90e5dbb4feacd64613a117dcfce565f11bc6
Sneha Shaw <snehashaw122@gmail.com>
2022-12-07 12:02:22 +0530

    `feat`:`shift replace form`

--- src/pages/ShiftReplace/ShiftReplace.js
+++ src/pages/ShiftReplace/ShiftReplace.js
@@ -2,13 +2,65 @@ import React from 'react'
 import useStyles from './styles'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import SubSidebar from '../../components/SubSidebar/SubSidebar'
+import { Button } from '@mui/material'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 const ShiftReplace = () => {
-    const classes = useStyles()
+  const classes = useStyles()
+  const options = [
+    'AM', 'PM'
+  ];
+  const defaultOption = options[0];
   return (
     <div className={classes.root}>
       <Sidebar />
       <SubSidebar />
+      {/* form : input(name of person who cannot work shifT,NAME OF PERSON WHO WILL work shift, date, start time) */}
+      <div className={classes.main}>
+        <h1>Shift Replace</h1>
+        <div className={classes.form}>
+          <div className={classes.formItem}>
+            <label htmlFor="name">Name of person who cannot work shift:</label>
+            <input type="text" id="name" />
+          </div>
+          <div className={classes.formItem}>
+            <label htmlFor="name">Name of person who will work shift:</label>
+            <input type="text" id="name" />
+          </div>
+          <div className={classes.formItem}>
+            <label htmlFor="date">Date:</label>
+            <input type="date" id="date" />
+          </div>
+          <div className={classes.formItem}>
+            <label htmlFor="time">Start Time:</label>
+            <div>
+
+              <input type="time" id="time" />
+              <Dropdown options={options} value={defaultOption} placeholder="Select an option" />
+            </div>
+          </div>
+          <div className={classes.formItem}>
+            <label htmlFor="time">End Time:</label>
+            <div>
+
+              <input type="time" id="time" />
+              <Dropdown options={options} value={defaultOption} placeholder="Select an option" />
+            </div>
+          </div>
+          {/* <div className={classes.formItem}>
+            <label htmlFor="reason">Reason</label>
+            <input type="text" id="reason" />
+            </div> */}
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Submit
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/ShiftReplace/styles.js
+++ src/pages/ShiftReplace/styles.js
@@ -5,5 +5,80 @@ export default makeStyles((theme) => ({
         display: 'flex',
         width: "100%",
         height: "100vh",
+        background: "#f7f7f7",
+    },
+    main:{
+        width: "55%",
+        height: "85%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        // justifyContent: "center",
+        margin: "3rem auto",
+        background: "#fff",
+        borderRadius: "1rem",
+        padding: "2rem",
+        // box shadow
+        boxShadow: "0 0 1rem 0 rgba(0, 0, 0, 0.1)",
+        '&>h1': {
+            fontSize: "3rem",
+            fontWeight: "bold",
+            marginBottom: "2rem",
+
+        },
+    },
+    form:{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        '&>button':{
+            width: "50%",
+            height: "3.5rem",
+            fontSize: "1.5rem",
+            fontWeight: "bold",
+            background: "#06283D",
+            color: "#fff",
+            marginTop: "1.5rem",
+            '&:hover':{
+                background: "rgba(6, 40, 61,0.9)",
+            }
+        }
+    },
+    formItem:{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        '&>div':{
+            width: "54%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            '&>input':{
+                width: "70%",
+            }
+        },
+        '& label':{
+            fontSize: "1.5rem",
+            // fontWeight: "bold",
+            margin: "1rem 0",
+            // capitalize
+            textTransform: "capitalize",
+        },
+        '& input':{
+            width: "50%",
+            height: "3rem",
+            fontSize: "1.5rem",
+            padding: "0 1rem",
+            borderBottom: "1px solid rgba(0, 0, 0, 0.2)",
+            border: "none",
+            outline: "none",
+            background: "transparent",
+        }
     }
 }))
