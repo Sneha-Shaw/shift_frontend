88052ad59ee077330a71385f02d444a6f41a8e12
Sneha Shaw <snehashaw122@gmail.com>
2022-12-08 15:01:36 +0530

    `feat`:`beak form`

--- src/pages/Break/Break.js
+++ src/pages/Break/Break.js
@@ -1,14 +1,133 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './styles'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import SubSidebar from '../../components/SubSidebar/SubSidebar'
+import { Button } from '@mui/material'
+import Switch from '@mui/material/Switch';
+import Timepicker from '../../components/Timepicker/TimePicker'
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 const Break = () => {
     const classes = useStyles()
+    // breaks array
+    const breaks = [
+        {
+            name: 'Lunch Break',
+            duration: '1:00',
+            start: '1:30',
+            end: '2:30',
+            automatic: true,
+            manual: false
+        },
+        {
+            name: 'Tea Break',
+            duration: '0:30',
+            start: '10:30',
+            end: '11:00',
+            automatic: false,
+            manual: true
+        },
+        {
+            name: 'Snack Break',
+            duration: '0:30',
+            start: '3:30',
+            end: '4:00',
+            automatic: false,
+            manual: true
+        },
+    ]
+    const options = [
+        'Manual', 'Automatic'
+    ];
+    const defaultOption = options[0];
+    const [show, setShow] = useState(true)
+
     return (
         <div className={classes.root}>
             <Sidebar />
             <SubSidebar />
+            <div className={classes.main}>
+                {/* header */}
+                <div className={classes.header}>
+                    <h1>Break Details</h1>
+                    <Button
+                        variant="contained"
+                        onClick={() => setShow(!show)}
+                    >
+                        Add Break
+                    </Button>
+                </div>
+                {
+                    show ? (
+                        <div className={classes.form}>
+                            <div className={classes.formItem}>
+                                <label htmlFor="breakName">Break Name:</label>
+                                <input type="text" id="breakName" />
+                            </div>
+                            <div className={classes.formItem}>
+                                <label htmlFor="duration">Duration:</label>
+                                <input type="text" id="duration" />
+                            </div>
+                            <div className={classes.formItemTime}>
+                                <Timepicker placeholder="Start Time" />
+                            </div>
+                            <div className={classes.formItemTime}>
+                                <Timepicker placeholder="End Time" />
+                            </div>
+                            <div className={classes.formItem}>
+                                <label htmlFor="type">Type:</label>
+                                <Dropdown
+                                    options={options}
+                                    value={defaultOption}
+                                    placeholder="Select an option"
+                                />
+                            </div>
+                            <div className={classes.formItem}>
+                                <Button
+                                    variant="contained"
+                                    onClick={() => setShow(!show)}
+                                >
+                                    Add Break
+                                </Button>
+                            </div>
+                        </div>
+                    )
+                        :
+                        <div className={classes.table}>
+                            <div className={classes.tableHeader}>
+                                <div className={classes.tableHeaderItem}>Break Name</div>
+                                <div className={classes.tableHeaderItem}>Duration</div>
+                                <div className={classes.tableHeaderItem}>Start Time</div>
+                                <div className={classes.tableHeaderItem}>End Time</div>
+                                <div className={classes.tableHeaderItem}>Type</div>
+                                <div className={classes.tableHeaderItem}>Status</div>
+                            </div>
+                            {breaks.map((breaks) => (
+                                <div className={classes.tableRow}>
+                                    <div className={classes.tableRowItem}>{breaks.name}</div>
+                                    <div className={classes.tableRowItem}>{breaks.duration}</div>
+                                    <div className={classes.tableRowItem}>{breaks.start}</div>
+                                    <div className={classes.tableRowItem}>{breaks.end}</div>
+                                    <div className={classes.tableRowItem}>
+                                        {
+                                            breaks.automatic ? <div>Automatic</div> : <div>Manual</div>
+                                        }
+                                    </div>
+                                    <div className={classes.tableRowItem}>
+                                        <Switch
+                                            defaultChecked
+                                            // color="default"
+                                            inputProps={{ 'aria-label': 'checkbox with default color' }}
+                                        />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                }
+
+            </div>
         </div>
     )
 }
--- src/pages/Break/styles.js
+++ src/pages/Break/styles.js
@@ -6,5 +6,157 @@ export default makeStyles((theme) => ({
         height: "100vh",
         display: "flex",
         background: "#f7f7f7",
+        position: "relative"
     },
+    main:{
+        width: "55%",
+        height: "fit-content",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        // justifyContent: "center",
+        margin: "8rem auto",
+
+        borderRadius: "1rem",
+        // padding: "2rem",
+        '& h1':{
+            fontSize: "3rem",
+            fontWeight: "bold",
+            marginBottom: "2rem",
+
+        }
+
+    },
+    header:{
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: "2rem",
+       
+        padding: "1rem",
+        // borderRadius: "0.5rem",
+        '&>button':{
+            background: "#fff",
+            color: "#000",  
+            border: "1px solid #000",
+            borderRadius: "0.5rem",
+            padding: "0.5rem 1rem",
+            fontSize: "1rem",
+            fontWeight: "bold",
+            '&:hover':{
+                background: "#000",
+                color: "#fff",
+            }
+        }
+    },
+    table:{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "2rem auto",
+        
+        background: "#fff",
+        // boxShadow: "0 0 1rem 0 rgba(0, 0, 0, 0.1)",
+    },
+    tableHeader:{
+        width: "100%",
+        display: "flex",
+        background: "#000",
+        color: "#fff",
+        // padding: ".5rem",
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+       
+    },
+    tableHeaderItem:{
+        width: "25%",
+        padding: "1rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        
+    },
+    tableRow:{
+        width: "100%",
+        display: "flex",
+        background: "#fff",
+        color: "#000",
+        fontSize: "1.3rem",
+        '&:nth-child(odd)':{
+            background: "#f7f7f7",
+        }
+
+    },
+    tableRowItem:{
+        width: "25%",
+        padding: "1rem",
+        border: "1px solid #000",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        '&:nth-child(4)':{
+            // width: "25%",
+            // display: "flex",
+            justifyContent: "space-between",
+        }
+    },
+    form:{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "2rem auto",
+        padding: "2rem",
+        background: "#fff",
+        boxShadow: "0 0 1rem 0 rgba(0, 0, 0, 0.1)",
+    },
+    formItem:{
+        width: "60%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: "1rem",
+        '& label':{
+            width: "20%",
+            fontSize: '1.5rem',
+            fontWeight: 'bold',
+            color: '#06383D',
+            marginRight: '1rem',
+        },
+        '& input':{
+            width: "70%",
+            height: "3rem",
+            fontSize: "1.5rem",
+            padding: "0 0.5rem ",
+            border: "1px solid #ccc",
+            borderRadius: "0.5rem",
+            '&:focus':{
+                outline: "none",
+                border: "1px solid #000",
+            }
+        },
+        '&>button':{
+            width: "20%",
+            height: "3rem",
+            // fontSize: "1.5rem",
+            fontWeight: "bold",
+            background: "#06283D",
+            margin: "auto" ,
+            '&:hover':{
+                background: "rgba(6, 40, 61,0.9)"
+            }
+        },
+        '&>div':{
+            width: "72%",
+            // height: "3rem",
+        }
+    },
+    formItemTime:{
+        width: "71%",
+    }
+
 }))
