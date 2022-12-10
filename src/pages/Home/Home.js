67b83150c81741871fa58a08ac510b1b40eae5a7
Sneha Shaw <snehashaw122@gmail.com>
2022-12-10 13:28:43 +0530

    `feat`:home`

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,71 @@
 import React from 'react'
 import useStyles from './styles'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-    const classes = useStyles()
+  const classes = useStyles()
+  const user = "Jane Doe"
+  const navigate = useNavigate()
+  const notifications = [
+    {
+      id: 1,
+      title: "Duty Replacement",
+      description: "Dr. jane have been assigned to replace Dr. John Doe on 12/12/2022",
+      date: "12/12/2022",
+      start: "8:00 AM",
+      end: "11:00 AM"
+    },
+    {
+      id: 2,
+      title: "Duty Replacement",
+      description: "Dr. jane have been assigned to replace Dr. John Doe on 12/12/2022",
+      date: "12/12/2022",
+      start: "8:00 AM",
+      end: "11:00 AM"
+    }
+  ]
+  const managerInfo= null
   return (
     <div className={classes.root}>
       <Sidebar />
+      {/* Wlcome */}
+      <div className={classes.main}>
+        <div className={classes.welcome}>
+          <h1>Welcome Back, {user}</h1>
+          {
+            managerInfo && <Button variant="contained" color="primary"
+            onClick={() => navigate('/attendance/doctors')}
+            >Add New Doctor</Button>
+          }
+           
+        </div>
+        {/* notifications */}
+        <div className={classes.notifications}>
+          <div className={classes.header}>
+            <h2>Latest Notifications (2)</h2>
+            <p
+              onClick={() => navigate('/notifications')}
+            >
+              See All
+            </p>
+          </div>
+          <div className={classes.notificationsList}>
+            {notifications.map((notification) => (
+              <div className={classes.notification} key={notification.id}>
+                <div className={classes.notificationHeader}>
+                  <h3>{notification?.title}</h3>
+                  <p>{notification?.date}</p>
+                </div>
+                <p>{notification.description}</p>
+                <p>{notification?.start} - {notification?.end}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,6 +5,102 @@ export default makeStyles((theme) => ({
         width: "100%",
         height: "100vh",
         display: "flex",
-        background: "#f7f7f7",
+        // background: "#f7f7f7",
     },
+    main: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        // justifyContent: "center",
+        padding: "2rem",
+    },
+    welcome: {
+        width: "60%",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        "&>h1": {
+            color: "#06283D",
+            fontSize: "2.4rem",
+            fontWeight: "bold",
+        },
+        "&>button": {
+            background: "#06283D",
+            color: "#fff",
+            fontSize: "1.4rem",
+            fontWeight: "bold",
+            padding: ".5rem 1rem",
+            // borderRadius: "2rem",
+            boxShadow: "0 0 10px 0 rgba(0,0,0,0.1)",
+            "&:hover": {
+                background: "#06283D",
+                color: "#fff",
+            },
+        },
+    },
+    notifications: {
+        width: "60%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "2rem 0",
+        "&>h2": {
+            color: "#06283D",
+            fontSize: "2.4rem",
+            fontWeight: "bold",
+        },
+    },
+    header: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        "&>p": {
+            color: "#06283D",
+            fontSize: "1.4rem",
+            fontWeight: "bold",
+            cursor: "pointer",
+        },
+    },
+    notificationsList: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        "&>div": {
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            background: "#fff",
+            padding: ".5rem 1rem",
+            borderRadius: "1rem",
+            boxShadow: "0 0 10px 0 rgba(0,0,0,0.1)",
+            margin: "2rem 0 1rem",
+            "&:last-child": {
+                marginBottom: 0,
+            },
+            "&>p": {
+                color: "#06283D",
+                fontSize: "1.4rem",
+                fontWeight: "bold",
+                margin: ".5rem",
+            },
+            "& h3": {
+               
+                color: "#06283D",
+                fontSize: "1.8rem",
+                fontWeight: "bold",
+                margin: "1rem 0",
+            }
+        }
+    },
+   
 }))
